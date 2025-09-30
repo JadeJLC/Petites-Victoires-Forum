e22074450c1e14006a8848d2401c1d929ecf1151
jcolange <jade.colanges@live.fr>
2025-09-30 16:49:41 +0200

    Affichage du profil modifié
    - Ajout d'un bouton pour choisir entre afficher ses propres posts et afficher les posts likés
    - Ajout d'une fonction back pour récupérer tous les posts likés par l'utilisateur

--- utils/getuserprofil.go
+++ utils/getuserprofil.go
@@ -43,6 +43,59 @@ func GetUserInfoFromSess(sessId string) (models.User, error) {
 	return user, nil
 }
 
+func GetUserLikes(userId int) ([]models.LastPost, error) {
+	var posts []models.LastPost
+	var post models.LastPost
+	db, err := sql.Open("sqlite3", "./data/forum.db")
+	if err != nil {
+		log.Printf("<getuserposts.go> Could not open database: %v\n", err)
+		return nil, err
+	}
+	defer db.Close()
+
+	sqlQuery := `
+        SELECT
+            l.message_id,
+            m.topic_id,
+            m.content,
+            m.created_at,
+            t.name
+        FROM like l
+		JOIN message m ON l.message_id = m.id
+        JOIN topic t ON m.topic_id = t.id
+		WHERE l.user_id = ?
+        ORDER BY m.created_at DESC
+    `
+
+	rows, err := db.Query(sqlQuery, userId)
+	if err != nil {
+		log.Printf("<getuserposts.go> Could not execute the sql query: %v\n", err)
+		return []models.LastPost{}, err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&post.MessageID, &post.TopicID, &post.Content, &post.Created, &post.TopicName); err != nil {
+			log.Printf("<getuserposts.go> Error scanning message row: %v\n", err)
+			return nil, err
+		}
+		post.Author, err = getdata.GetUserInfoFromID(db, userId)
+		if err != nil {
+			log.Printf("<getuserposts.go> Could not execute GetUserInfoFromID: %v\n", err)
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Printf("<getuserposts.go> Error during rows iteration: %v\n", err)
+		return nil, err
+	}
+
+	return posts, nil
+
+}
+
 func GetUserPosts(userId int) ([]models.LastPost, error) {
 	var posts []models.LastPost
 	var post models.LastPost
